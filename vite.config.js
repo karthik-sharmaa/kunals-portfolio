e5ff2191fdc58e776848a1d429a1d6ac06941a27
KUNAL VERMA <kayvermaofficial@gmail.com>
2023-05-28 22:37:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
+const path = require('path');
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import minify from 'vite-plugin-minify';
+module.exports = {
+  root: 'src',
 
+  plugins: [
+    minify({
+      removeComments: true,
+    }),
+  ],
+
+  build: {
+    outDir: '../dist',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+    },
+  },
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
   
 })
+
