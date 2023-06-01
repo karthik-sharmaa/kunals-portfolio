8fc473b1c7f42e1c991cd514f3f9da2c48d197a8
KUNAL VERMA <kayvermaofficial@gmail.com>
2023-06-01 16:17:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -9,7 +10,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/js/main.js'), // Path to main.js file
-        app: resolve(__dirname, 'src/main.jsx'), // Path to main.jsx file
+        app: resolve(__dirname, 'src/main.jsx'),
+        index: resolve(__dirname, 'src/index.css'), // Path to main.jsx file
       },
     },
   },
